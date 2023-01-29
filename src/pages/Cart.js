5c8410373cbe6cf8c7b2119b04120e975f4fdaf3
geisniwander <geisniwander@gmail.com>
2023-01-29 17:01:24 -0300

    feat: displaying shopping cart dynamically

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,160 +1,153 @@
 import styled from "styled-components";
 import Header from "../components/Header";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import AuthProvider from "../AppContext/auth";
+import { CartContext } from "../AppContext/CartContext.js";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 
 //caso carrinho vazio -> chamar WrapperBlank
 //se não WrapperPurchase
 
+export default function Cart() {
+  const { token } = useContext(AuthProvider);
+  const [ cart, setCart ] = useState([]);
 
-export default function Cart(){
-    return(
-        <>
-        <Header/>
-        
-        {/* <WrapperBlank>
+  useEffect(() => {
+    async function findCart() {
+      try {
+        const getCartURL = `${process.env.REACT_APP_API_URL}/getCart`;
+        const res = await axios.get(getCartURL, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setCart(res.data.products);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    findCart();
+  }, []);
+
+if(!cart) return "h"
+
+  return (
+    <>
+      <Header />
+      {/* <WrapperBlank>
         <p>Seu carrinho está vazio</p>
         <ButtonGoBack>Volte para ver produtos</ButtonGoBack>
         </WrapperBlank> */}
 
-        <WrapperPurchase>
+      <WrapperPurchase>
         <ContainerPurchase>
-        <StyledTitle><p>Seu carrinho:</p></StyledTitle>
+          <StyledTitle>
+            <p>Seu carrinho:</p>
+          </StyledTitle>
 
+          {cart.map((product) => (
             <ProductChosen>
-                <ProductImage></ProductImage>
-                <ProductDescription>Camisa polo extra giga algodão</ProductDescription>
-                <ProductPrice>R$ 1900,00</ProductPrice>
+              <ProductImage src={product.newProduct.pictures[0]}></ProductImage>
+              <ProductDescription>{product.newProduct.name}</ProductDescription>
+              <ProductPrice>{`R$ ${product.newProduct.value}`}</ProductPrice>
             </ProductChosen>
-            <ProductChosen>
-                <ProductImage></ProductImage>
-                <ProductDescription>Camisa polo extra giga algodão</ProductDescription>
-                <ProductPrice>R$ 1900,00</ProductPrice>
-            </ProductChosen>
-            <ProductChosen>
-                <ProductImage></ProductImage>
-                <ProductDescription>Camisa polo extra giga algodão</ProductDescription>
-                <ProductPrice>R$ 1900,00</ProductPrice>
-            </ProductChosen>
-            <ProductChosen>
-                <ProductImage></ProductImage>
-                <ProductDescription>Camisa polo extra giga algodão</ProductDescription>
-                <ProductPrice>R$ 1900,00</ProductPrice>
-            </ProductChosen>
-            <ProductChosen>
-                <ProductImage></ProductImage>
-                <ProductDescription>Camisa polo extra giga algodão</ProductDescription>
-                <ProductPrice>R$ 1900,00</ProductPrice>
-            </ProductChosen>
-            <ProductChosen>
-                <ProductImage></ProductImage>
-                <ProductDescription>Camisa polo extra giga algodão</ProductDescription>
-                <ProductPrice>R$ 1900,00</ProductPrice>
-            </ProductChosen>
+          ))}
 
-            
-          
         </ContainerPurchase>
-        
-        <ContainerPurchase>
 
-        <WrapperTotal>
+        <ContainerPurchase>
+          <WrapperTotal>
             <h1>Total:</h1>
             <p>R$20000,00</p>
-         </WrapperTotal>
+          </WrapperTotal>
 
-        <NavLink to="/checkout">
-        <ButtonFinish>Finalizar compra</ButtonFinish>
-        </NavLink>
+          <NavLink to="/checkout">
+            <ButtonFinish>Finalizar compra</ButtonFinish>
+          </NavLink>
         </ContainerPurchase>
-
-        </WrapperPurchase>
-
-        
-        </>
-    )
+      </WrapperPurchase>
+    </>
+  );
 }
 
 const WrapperBlank = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-margin-top: 150px;
-p{
-    font-size:40px;
-}
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 150px;
+  p {
+    font-size: 40px;
+  }
+`;
 
 const ButtonGoBack = styled.button`
-height: 58px;
-width: 326px;
-background-color: #ee6c4d;
-border: 1px solid #ee6c4d;
-border-radius: 5px;
-color: #fff;
-font-size: 20px;
-margin-top: 20px;
-`
+  height: 58px;
+  width: 326px;
+  background-color: #ee6c4d;
+  border: 1px solid #ee6c4d;
+  border-radius: 5px;
+  color: #fff;
+  font-size: 20px;
+  margin-top: 20px;
+`;
 const WrapperPurchase = styled.div`
-display: flex;
-width:1000px;
-`
+  display: flex;
+  width: 1000px;
+`;
 const StyledTitle = styled.div`
-p{
+  p {
     font-size: 40px;
     margin-top: 50px;
-   
-}
-`
+  }
+`;
 const ContainerPurchase = styled.div`
-margin-bottom: 100px;
-`
+  margin-bottom: 100px;
+`;
 
 const ProductChosen = styled.div`
-width: 900px;
-display: flex;
-margin-top: 20px;
-align-items: center;
-`
+  width: 900px;
+  display: flex;
+  margin-top: 20px;
+  align-items: center;
+`;
 
-const ProductImage = styled.div`
-width: 80px;
-height: 100px;
-background-color: #98C1D9;
-`
+const ProductImage = styled.img`
+  width: 80px;
+  height: 100px;
+  background-color: #98c1d9;
+`;
 
 const ProductDescription = styled.div`
-width: 500px;
-word-break: break;
-font-size: 15px;
-margin-left: 20px;
-`
+  width: 500px;
+  word-break: break;
+  font-size: 15px;
+  margin-left: 20px;
+`;
 
 const ProductPrice = styled.div`
-width: 600px; 
-font-size: 15px;
-
-`
+  width: 600px;
+  font-size: 15px;
+`;
 
 const ButtonFinish = styled.button`
-width: 300px;
-height: 50px;
-color: #fff;
-background-color: #F06969;
-border-radius: 10px;
-border: 1px solid #F06969;
-margin-top: 500px;
-font-size: 20px;
-`
+  width: 300px;
+  height: 50px;
+  color: #fff;
+  background-color: #f06969;
+  border-radius: 10px;
+  border: 1px solid #f06969;
+  margin-top: 500px;
+  font-size: 20px;
+`;
 
 const WrapperTotal = styled.div`
-margin-top: 150px;
-h1{
+  margin-top: 150px;
+  h1 {
     font-weight: 700;
     margin-bottom: 5px;
-}
-`
+  }
+`;
 
 const NavLink = styled(Link)`
-text-decoration: none;
-color: #000;
-`
+  text-decoration: none;
+  color: #000;
+`;
